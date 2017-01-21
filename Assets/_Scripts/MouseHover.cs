8dfa4bbe5e181c02ff6fdbe719bb0631d2010efa
Captwalloper <comccoy@cfl.rr.com>
2017-01-21 15:35:18 -0500

    1st commita

---
+++ Assets/_Scripts/MouseHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+    private Color startColor;
+    private static readonly Color hoverColor = Color.red;
+
+    void Start()
+    {
+        startColor = GetComponent<Renderer>().material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startColor;
+    }
+}
