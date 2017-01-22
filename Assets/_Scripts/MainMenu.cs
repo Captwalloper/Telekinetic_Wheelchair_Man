7835ba4057c93e3d1c5bad0a1b124d7445a2a331
Captwalloper <comccoy@cfl.rr.com>
2017-01-21 21:12:21 -0500

    Main Menu

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    private Dictionary<string, Action> ActionToTake;
+    //
+    private MenuItem item;
+
+    void OnEnable()
+    {
+        ActionToTake = new Dictionary<string, Action>()
+        {
+            { "Start", StartGame }
+            , { "Quit", QuitGame }
+        };
+
+        item = GetComponent<MenuItem>();
+        item.Activated += OnActivated;
+    }
+
+    void OnDisable()
+    {
+        item.Activated -= OnActivated;
+    }
+
+    private void OnActivated(string name)
+    {
+        Action action = ActionToTake[name];
+        action();
+    }
+
+    private void StartGame()
+    {
+        return;
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Scripts/MenuItem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuItem : MonoBehaviour {
+    public event Action<string> Activated;
+    //
+    private const string menuItemTag = "MenuItem";
+    //
+    private static string nameOfSelected = null;
+    private Color startColor;
+    private static readonly Color hoverColor = Color.red;
+    //
+    private static SteamVR_LaserPointer pointer;
+    private static SteamVR_TrackedController controller;
+
+    void OnEnable()
+    {
+        startColor = GetComponent<Renderer>().material.color;
+
+        pointer = pointer ?? GameObject.FindObjectOfType<SteamVR_LaserPointer>();
+        pointer.PointerIn += OnPointerIn;
+        pointer.PointerOut += OnPointerOut;
+
+        controller = controller ?? GameObject.FindObjectOfType<SteamVR_TrackedController>();
+        controller.TriggerClicked += OnTriggerPressed;
+    }
+
+    void OnDisable()
+    {
+        pointer.PointerIn -= OnPointerIn;
+        pointer.PointerOut -= OnPointerOut;
+
+        controller.TriggerClicked -= OnTriggerPressed;
+    }
+
+    private void OnPointerIn(object sender, PointerEventArgs e)
+    {
+        if (!IsMenuItem(e.target))
+        {
+            return;
+        }
+        else
+        {
+            if (!TargetsScriptObject(e.target))
+            {
+                return;
+            }
+            else
+            {
+                Select(e.target.name);
+            }
+        }
+    }
+
+    private void OnPointerOut(object sender, PointerEventArgs e)
+    {
+        if (IsMenuItem(e.target) && TargetsScriptObject(e.target))
+        {
+            DeSelect(e.target.name);
+        }
+    }
+
+    private bool triggerDebounced = false;
+    private void OnTriggerPressed(object sender, ClickedEventArgs e)
+    {
+        triggerDebounced = !triggerDebounced;
+        if (nameOfSelected != null && triggerDebounced) // A MenuItem is currently selected
+        {
+            Activated(nameOfSelected); // Fire event
+        }
+    }
+
+    private bool IsMenuItem(Transform target)
+    {
+        return target.tag == menuItemTag;
+    }
+
+    private bool TargetsScriptObject(Transform target)
+    {
+        return target.name == gameObject.name;
+    }
+
+    private void Select(string name)
+    {
+        nameOfSelected = name;
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    private void DeSelect(string name)
+    {
+        nameOfSelected = null;
+        GetComponent<Renderer>().material.color = startColor;
+    }
+}
