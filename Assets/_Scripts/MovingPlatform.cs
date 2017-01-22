696a323ad81207ffb34df7b3c23e2736564ac719
Captwalloper <comccoy@cfl.rr.com>
2017-01-22 03:22:43 -0500

    Wheelchair scaled, with physics

---
+++ Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    public Transform movingPlatform;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+    private State? currentState = null;
+    public float smooth;
+    public float resetTime;
+
+    void Start()
+    {
+        ChangeTarget();
+    }
+
+    void FixedUpdate()
+    {
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, smooth * Time.deltaTime);
+    }
+
+    void ChangeTarget()
+    {
+        if (currentState.HasValue && currentState.Value == State.MovingToPosition1)
+        {
+            currentState = State.MovingToPosition2;
+            newPosition = position2.position;
+        }
+        else if (currentState.HasValue && currentState == State.MovingToPosition2)
+        {
+            currentState = State.MovingToPosition1;
+            newPosition = position1.position;
+        }
+        else
+        {
+            currentState = State.MovingToPosition2;
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+
+    private enum State
+    {
+        MovingToPosition1,
+        MovingToPosition2,
+    }
+}
